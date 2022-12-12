962a88fe2b372ff6c35745c4d0d48d207e39fd08
StefaniaSperani <thedarkcry@hotmail.it>
2022-12-12 16:11:19 +0100

    functions + bonus1

--- Models/Movie.php
+++ Models/Movie.php
@@ -3,18 +3,30 @@
 class Movie
 {
     //variabili comuni con le istanze
-    public $name;
-    public $year;
-    public $description;
+    private $name;
+    private $year;
+    public $genres;
+    private $description;
     public $cast;
 
 
-    function __construct(string $_name, int $_year, string $_description, array $_cast = [])
+    function __construct(string $_name, int $_year, array $_genres, string $_description, array $_cast = [])
     {
         $this->name = $_name;
         $this->year = $_year;
+        $this->genres = $_genres;
         $this->description = $_description;
         $this->cast = $_cast;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getInfo()
+    {
+        return $this->name . '<br>' . $this->year . '<br>' . implode(' ', $this->genres) . '<br>' . $this->description . '<br>' . implode(' ', $this->cast);
     }
 
 
--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 include_once __DIR__ . '/Models/Movie.php';
-$movie1 = new Movie('Matrix', 1999, 'Lorem ipsum', ['pippo', 'pluto', 'paperino']);
+$movie1 = new Movie('Film1', 1999, ['action', 'fantasy'], 'Lorem ipsum dolor sit amet consectetur', ['pippo', 'pluto', 'paperino']);
 var_dump($movie1);
 
+echo $movie1->getDescription();
+echo '<br>';
+echo $movie1->getInfo();
+
+$movie2 = new Movie('Film2', 2020, ['horror', 'thriller'], 'Quas quam voluptates deserunt ipsam?');
+var_dump($movie2);
+
+echo $movie2->getDescription();
+echo '<br>';
+echo $movie2->getInfo();
 
 
 
